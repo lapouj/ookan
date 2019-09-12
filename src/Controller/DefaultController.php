57f412e1f9be2a6bc9ec3835e0bcfbfb149789a7
StephHhhhh <50447270+StephHhhhh@users.noreply.github.com>
2019-09-12 17:17:09 +0200

    connexion

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\User; // Intéraction
+
 class DefaultController extends AbstractController
 {
     /**
@@ -19,14 +21,59 @@ class DefaultController extends AbstractController
 
     public function connect()
     {
+
+        $errors = [];
+
+        $success = '';
+        
+
+        if (!empty($_POST['email']) && !empty($_POST['password'])) {
+
+            $safe = array_map('trim', array_map('strip_tags', $_POST));
+        
+            $userData = $this->getDoctrine()->getRepository(User::class)->findBy(['email' => $safe['email']]);
+            
+            foreach ($$_POST as $key => $value) {
+                $post[$key] = trim(strip_tags($value));
+            }
+
+            if (empty($post['email'])) {
+                $errors[] = 'Veuillez saisir votre email';
+            }
+
+            if (empty($post['password'])) {
+                $errors[] = 'Veuillez saisir votre mot de passe';
+            }
+
+            if (count($errors) == 0) {
+                if (password_verify($post['password'], $userData->getPwd())) {
+
+                    $_SESSION['user'] = $userData;
+                    session_regenerate_id();                    
+                    $mes_erreurs = true;
+                    
+                    $em->persist($userData);
+                    //éxecution
+                    $em->flush();
+                    //return $this->redirectToRoute('accueil');
+                
+                    $success = 'Connexion réussi !';
+
+                }
+
+
+            } else {
+
+                $errors[] = 'Le mot de passe ne correspond pas';
+            }
+        }
         return $this->render('connexion.html.twig', [
-            'controller_name' => 'DefaultController',
+            
         ]);
     }
     public function mentions()
     {
         return $this->render('mentions.html.twig', [
-            'controller_name' => 'DefaultController',
         ]);
     }
 }
--- src/Controller/UserprofileController.php
+++ src/Controller/UserprofileController.php
@@ -22,6 +22,7 @@ class UserprofileController extends AbstractController
 
         return $this->render('userprofile/user-profile.html.twig', [
             // 'user' => $user,
+            "buisness_list" => $buisness_list,
         ]);
     }
 }
