a6bb0da46faeaf16ccc1e7df5c38b660b61d3bbf
Moukrenne <f.zorilla@gmail.com>
2019-09-16 12:02:51 +0200

    Update DefaultController.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -22,106 +22,69 @@ class DefaultController extends AbstractController
         ]);
     }
 
-    public function connect()
-    {
+public function connect()
+{
 
-        $errors = [];
+    $errors = [];
 
-        if (!empty($_POST)) {
-            $errors[] = 'Veuillez renseigner tous les champs';
-            
+    if (!empty($_POST)) {
+        $errors[] = 'Veuillez renseigner tous les champs';
+
 
         if (!empty($_POST['email']) || !empty($_POST['password'])) {
 
-        $safe = array_map('trim', array_map('strip_tags', $_POST));
+            $safe = array_map('trim', array_map('strip_tags', $_POST));
 
             if (!empty($safe['email'])) {
 
                 if(!filter_var($safe['email'], FILTER_VALIDATE_EMAIL)) {
 
-                    $errors[] = 'Votre adresse email n\'est pas valide';
+                $errors[] = 'Votre adresse email n\'est pas valide';
                 }
             }   
 
+            if (empty($safe['password'])) {
 
-            if (!empty($safe['password'])) {
+            $errors[] = 'Veuillez saisir votre mot de passe';
 
-                $errors[] = 'Veuillez saisir votre mot de passe';
             } 
 
 
-            $my_user_name = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $safe['email']]);
+            $userdata = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $safe['email']]);
 
+            $mailfound = 0;
 
-           $mailfound = 0;
-           if ($my_user_name) 
-           {
-               $mailfound = $my_user_name->getEmail();
-           }
-           else 
-           {
-               $errors[] = 'Utilisateur introuvable';
-           }
-           
-      
+            if ($userdata){
+                $mailfound = $my_user_name->getEmail();
+            }
+            else{
+                $errors[] = 'Utilisateur introuvable';
+            }
 
 
             if (count($errors) == 0) {
 
                 $errors = array_filter($errors);
 
-                
                 if(!empty($my_user_name)){
-
-                    $infos_session = [
-                        'id'            => $my_user_name->getId(),
-                        'email'         => $my_user_name->getEmail(),
-                        'username'      => $my_user_name->getUsername(),
-                    ];
-
-                    $session = new Session();
-                    $session->set('user', $infos_session);
-
-                    
-                    // Affichage (exemple)
-                    /*$user_en_session = $session->get('user');
-                    echo $user_en_session['email']; */
-                    return $this->redirectToRoute('user_profile');
-                }
-
-            if (password_verify($safe['password'], $infos_session->getPassword())) {
-                  
-              }  
-            }      
-        } 
-    }
-        
-        return $this->render('connexion.html.twig', [
-            'mes_erreurs'     =>  $errors,    
-        ]);
-    }
-
-
-
-
-
-         if (count($errors) == 0) {
-
-            $errors = array_filter($errors);
-
-
-            if(!empty($my_user_name)){
 
                 $session = new Session();
                 $session->set('pseudo',  $my_user_name->getPseudo());
 
                 return $this->redirectToRoute('user_profile');
 
-            }  
-        }      
+                }  
+            }   
+
+
+            return $this->render('connexion.html.twig', [
+            'mes_erreurs'     =>  $errors,    
+            ]);
+        }
+
     }
     return $this->render('connexion.html.twig', [
-        'mes_erreurs'     =>  $errors,    
+    'mes_erreurs'     =>  $errors,    
     ]);
 }
 
