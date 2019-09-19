4a0efec85828791b84bb08e44131e3594f6932a9
Remyschoenberger <52195604+Remyschoenberger@users.noreply.github.com>
2019-09-19 10:08:14 +0200

    modif form incrip'.

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -37,11 +37,11 @@ class InscriptionController extends AbstractController
     		// je nettoie les données reçues
     		$safe = array_map('trim', array_map('strip_tags', $_POST));
 
-            if (strlen($safe['nom']) <= 2) {
+            if (strlen($safe['nom']) <= 1) {
                 $errors[] = 'Votre nom doit contenir au moins 2 caractères';
             }
 
-            if (strlen($safe['prenom']) <= 2) {
+            if (strlen($safe['prenom']) <= 1) {
                 $errors[] = 'Votre prénom doit contenir au moins 2 caractères';
             }
 
